864eb5c3c8267bc689a0cdeb2d93e7f7f3358fc8
George-SF <141727609+George-SF@users.noreply.github.com>
2023-08-08 13:43:43 +0100

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Streamlit Training Level 1');
+streamlit.title('Streamlit Training Level 1')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
