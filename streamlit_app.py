2f00fd568e453e3ba86f492d1840a73293a6c5da
George-SF <141727609+George-SF@users.noreply.github.com>
2023-08-10 14:14:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ v_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 # set the index as the fruit name
 v_fruit_list = v_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include (preselects Acocado and Strawberries)
+# Let's put a pick list here so they can pick the fruit they want to include (preselects Avocado and Strawberries)
 # streamlit.multiselect("Pick some fruits:", list(v_fruit_list.index),['Avocado','Strawberries']) 
 # the above works as is, but we can assign to a variable to load the selected values into it:
 v_fruits_selected = streamlit.multiselect("Pick some fruits:", list(v_fruit_list.index),['Avocado','Strawberries'])
@@ -30,9 +30,12 @@ streamlit.dataframe(v_fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+# display an input box, with Kiwi defaulted in
+fruit_choice = streamlit.text_input('What fruite would you like information about?','Kiwi')
+streamlit.write('The user entered',fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 # take the json version of the response and normalise it using a pandas funciton
